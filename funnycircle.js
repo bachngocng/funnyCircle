2a606a851358b6664a96bf1ce87b03f527ae00c7
bachngocng <nnbach97@gmail.com>
2021-12-23 11:44:06 +0700

    done

---
+++ funnycircle.js
@@ -0,0 +1,13 @@
+class Circle {
+    x;
+    y;
+    radius;
+    color;
+
+    constructor(x,y,radius,color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+}
